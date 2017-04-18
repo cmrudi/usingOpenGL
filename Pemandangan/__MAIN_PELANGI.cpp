21b84892838291e8fd5e18c968f115d8c4d53427
Faza Thirafi <faza.thirafi@gmail.com>
2017-04-19 00:53:54 +0700

    merging objects

--- Pemandangan/__MAIN_PELANGI.cpp
+++ Pemandangan/__MAIN_PELANGI.cpp
@@ -68,7 +68,7 @@ void verColor(char a) {
   		glColor3f(1.0f, 1.0f, 1.0f);
 	  }
     else if(a=='o') {
-      glColor3f(0.49f, 0.59f, 0.807f); 
+      glColor3f(0.5f, 0.5f, 6.0f); 
     }
 }
 
@@ -244,21 +244,21 @@ void display(){
 ////////////////////////////////END OF RAINBOW
 
 ////////////////////////////////DRAWING MOUNTAIN
-	glBegin(GL_POLYGON);
-		glColor3f(0.1,0.9,0.1);
-		glVertex2f(-1,-0.25);
-		glVertex2f(-0.9,-0.2);
-		glVertex2f(-0.8,-0.2);
-		glVertex2f(-0.7,-0.2);
-		glVertex2f(-0.6,-0.25);
-		glVertex2f(-0.5,-0.35);
-		glVertex2f(-0.4,-0.45);
-		glVertex2f(-0.3,-0.45);
-		glVertex2f(-0.2,-0.50);
-		glVertex2f(-0.1,-0.55);
-		glVertex2f(0,-1);
-		glVertex2f(-1,-1);
-	glEnd();
+	// glBegin(GL_POLYGON);
+	// 	glColor3f(0.1,0.9,0.1);
+	// 	glVertex2f(-1,-0.25);
+	// 	glVertex2f(-0.9,-0.2);
+	// 	glVertex2f(-0.8,-0.2);
+	// 	glVertex2f(-0.7,-0.2);
+	// 	glVertex2f(-0.6,-0.25);
+	// 	glVertex2f(-0.5,-0.35);
+	// 	glVertex2f(-0.4,-0.45);
+	// 	glVertex2f(-0.3,-0.45);
+	// 	glVertex2f(-0.2,-0.50);
+	// 	glVertex2f(-0.1,-0.55);
+	// 	glVertex2f(0,-1);
+	// 	glVertex2f(-1,-1);
+	// glEnd();
 	
 	glBegin(GL_POLYGON);
 		glColor3f(0.1,0.9,0.1);
@@ -271,19 +271,6 @@ void display(){
 		glVertex2f(1,-1);
 	glEnd();
 
-
-glBegin(GL_POLYGON);
-   glColor3f(1.0f, 1.0f, 0.0f);
-    glVertex2f(0.150,0.400);
-   glColor3f(1.0f, 1.0f, 0.0f);
-    glVertex2f(0.450,0.400);
-
-   glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex2f(0.400,0.300);
-   glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex2f(0.200,0.300);
-
-  glEnd();
 
 
 	glBegin(GL_POLYGON);            // These vertices form a closed polygon
